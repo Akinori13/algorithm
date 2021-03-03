fcb6121df7a6ff9c8afde429280127001ada7ef3
akino <akikoba2001@gmail.com>
2021-03-03 11:59:22 +0900

    commit

---
+++ ABC183.py
@@ -0,0 +1,30 @@
+# AtCoder Beginner Contest 183
+# A-ReLU
+# x = int(input())
+
+# def main(value:int) -> int:
+#     if value >= 0:
+#         return value
+#     else:
+#         return 0
+
+# print(main(x))
+
+
+
+
+# B-Billiards
+# SG = list(map(int, input().split()))
+# point1 = [SG[0], SG[1]]
+# point2 = [SG[2], SG[3]]
+
+# def solve(point1:list, point2:list) -> float:
+#     ans = point1[0] + ( point1[1] / (point1[1]+point2[1]) ) * (point2[0]-point1[0])
+#     return ans
+
+# print(solve(point1, point2))
+
+
+
+
+# C-Travel
